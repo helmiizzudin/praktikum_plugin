2b38f218ad8566fc36e13ce3d2dd4cfe8199e43e
helmiizzuddin <helmigithub@gmail.com>
2024-11-05 13:08:52 +0700

    plugin

---
+++ README.md
@@ -0,0 +1,8 @@
+# plugin
+
+# Nama: Helmi izzuddin
+# NIM: 362358302109
+# Kelas: 2D
+
+![Screenshoot](assets/images/foto.jpg)
+![Screenshoot](assets/images/Screenshot 2024-11-05 113320.jpg)
---
+++ lib/displayPictureScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'dart:io';
+
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Display the Picture'),
+      ),
+      body: Center(
+        child: Image.file(File(imagePath)),
+      ),
+    );
+  }
+}
